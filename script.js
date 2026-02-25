059571752a7609999896a2c478a3a5bd7b27e7d4
Abdul Majeed Salim <majuprojects2025@gmail.com>
2026-02-25 12:11:14 +0530

    added index code

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function searchRecommendation() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
 
+    if (input.includes("beach")) {
+        document.getElementById("beach").scrollIntoView();
+    } else if (input.includes("temple")) {
+        document.getElementById("temple").scrollIntoView();
+    } else if (input.includes("japan") || input.includes("country")) {
+        document.getElementById("country").scrollIntoView();
+    } else {
+        alert("No recommendation found.");
+    }
+}
+
+function resetSearch() {
+    document.getElementById("searchInput").value = "";
+    window.scrollTo(0, 0);
+}
